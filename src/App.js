6a6749814b178bae3863f3aaf74fb54d2522cf13
Ramon <ramonomello@gmail.com>
2020-04-16 07:53:44 -0300

    Desafío concluido
    Criados os métodos para Adicionar, Remover e Listar os repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post('/repositories', {
+      title: "GoStack 3",
+      url: "ramon.mello.com",
+      techs: "cerebro"
+    });
+    console.log(repository.data);
+    console.log(repositories);
+    setRepositories([...repositories, repository.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    const repository = repositories.find(
+      repository => repository.id === id
+    );
+    const filterRepositories = repositories.filter(item => item.id !== repository.id)
+    
+    setRepositories(filterRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <strong>{repository.title}</strong>
+            <p>repository.url</p>
+            <p>repository.techs</p>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
